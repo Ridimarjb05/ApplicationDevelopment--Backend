ef6ef5900c2f8eb5db0be7b796f602928121de4e
Ridimarjb05 <ridimarjb2005@gmail.com>
2026-04-27 22:36:09 +0545

    add repository interface

---
+++ VehicleParts.Application/Interface/IRepository/IServiceReviewRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleParts.Application.Interface.IRepository
+{
+    internal class IServiceReviewRepository
+    {
+    }
+}
