f3e2abfde501a05a9e96f254e939e0cd706eca01
rajnishcdry <rajnishch456@gmail.com>
2026-04-27 20:57:48 +0545

    Implemented Feature 7 (Sales) and Feature 16 (Loyalty Discount)

---
+++ VehicleParts.API/Controllers/SaleRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public class SaleRequest
+{
+    public int CustomerId { get; set; }
+    public List<SaleItem> Items { get; set; }
+}
+
+public class SaleItem
+{
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ VehicleParts.API/Controllers/SalesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VehicleParts.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SalesController : ControllerBase
+{
+    private static List<Sale> sales = new List<Sale>();
+
+    [HttpPost("create-sale")]
+    public IActionResult CreateSale([FromBody] SaleRequest request)
+    {
+        decimal total = request.Items.Sum(i => i.Price * i.Quantity);
+
+        decimal discount = 0;
+
+        // 🔥 Feature 16: Loyalty logic
+        if (total > 5000)
+        {
+            discount = total * 0.10m;
+        }
+
+        decimal finalAmount = total - discount;
+
+        var sale = new Sale
+        {
+            Id = sales.Count + 1,
+            CustomerId = request.CustomerId,
+            TotalAmount = total,
+            Discount = discount,
+            FinalAmount = finalAmount,
+            Date = DateTime.Now
+        };
+
+        sales.Add(sale);
+
+        return Ok(sale);
+    }
+}
